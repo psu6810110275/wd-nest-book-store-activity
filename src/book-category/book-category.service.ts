ca3952a781345b068c49d97b08a69aa6216ad33d
Manattee Vilairat <manattee2998@gmail.com>
2025-12-17 19:05:27 +0700

    add Data Seeding(Fixtures)

--- src/book-category/book-category.service.ts
+++ src/book-category/book-category.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookCategoryDto } from './dto/create-book-category.dto';
 import { UpdateBookCategoryDto } from './dto/update-book-category.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BookCategory } from './entities/book-category.entity';
 
 @Injectable()
 export class BookCategoryService {
+  
+  constructor(
+    @InjectRepository(BookCategory)
+    private readonly bookCategoryRepository: Repository<BookCategory>,
+  ) {}
+
+  async onModuleInit() {
+    const count = await this.bookCategoryRepository.count();
+    if (count === 0) {
+      console.log('🌱 Seeding Book Categories...');
+      await this.bookCategoryRepository.save([
+        { name: 'Fiction', description: 'Stories and novels' },
+        { name: 'Technology', description: 'Computers and engineering' },
+        { name: 'History', description: 'Past events' },
+      ]);
+      console.log('✅ Seeding Completed!');
+    }
+  }
+
   create(createBookCategoryDto: CreateBookCategoryDto) {
     return 'This action adds a new bookCategory';
   }
-
+  
   findAll() {
     return `This action returns all bookCategory`;
   }
@@ -23,4 +45,7 @@ export class BookCategoryService {
   remove(id: number) {
     return `This action removes a #${id} bookCategory`;
   }
+
+  
+
 }
